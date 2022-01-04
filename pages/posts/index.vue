0b443c660048f8f6e8066b3261f6627bdc3fa3f2
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-04 21:50:45 +0200

    craete posts and validate our routes.params

---
+++ pages/posts/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="post">
+    <input type="text" v-model="postId" />
+    <button @click="loadPost">Get post</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    postId: "",
+  }),
+  methods: {
+    loadPost() {
+      this.$router.push(`/posts/${this.postId}`);
+    },
+  },
+};
+</script>
+
+<style></style>
