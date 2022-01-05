66e20021fc4d8e5e8c99d502c111497dfc0e6e20
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-05 16:52:23 +0200

    feat: add information on main page

---
+++ components/Promo.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="promo">
+    <div class="container">
+      <h1 class="title">My ssr blog! With Nuxt.js</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam consequuntur libero quaerat iure eveniet ut harum ullam eaque praesentium recusandae.</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Promo",
+};
+</script>
+
+<style lang="scss">
+.promo {
+  text-align: center;
+
+  p {
+    color: #999;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="wrapper-content wrapper-content--fixed"></div>
+  <div class="wrapper-content wrapper-content--fixed">
+    <Promo />
+    <Intro title="My lasts posts"/>
+  </div>
 </template>
 
 <script>
+import Promo from '@/components/Promo.vue';
+import Intro from '@/components/UI/Intro.vue';
+
 export default {
   name: "IndexPage",
+  components: {
+    Promo,
+    Intro,
+  }
 };
 </script>
