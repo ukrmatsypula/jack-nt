2919a7897a84141effac6c63071feb3291a98a1a
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-05 18:09:11 +0200

    feat: render post in main page

---
+++ components/blog/PostsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="post-list">
+    <div class="container">
+      <div class="posts-wrapper">
+        <PostPreview v-for="post in posts" :key="post.id" :post="post" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import PostPreview from "@/components/blog/PostPreview.vue";
+
+export default {
+  name: "PostsList",
+  components: {
+    PostPreview,
+  },
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.posts-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,44 @@
 <template>
   <div class="wrapper-content wrapper-content--fixed">
     <Promo />
-    <Intro title="My lasts posts"/>
+    <Intro title="My lasts posts" />
+    <PostsList :posts="posts" />
   </div>
 </template>
 
 <script>
-import Promo from '@/components/Promo.vue';
-import Intro from '@/components/UI/Intro.vue';
+import Promo from "@/components/Promo.vue";
+import Intro from "@/components/UI/Intro.vue";
+import PostsList from "@/components/blog/PostsList";
 
 export default {
   name: "IndexPage",
   components: {
     Promo,
     Intro,
-  }
+    PostsList,
+  },
+  data: () => ({
+    posts: [
+      {
+        id: 1,
+        title: "1 post",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        img: "https://cdn.shopify.com/s/files/1/0526/8740/5250/products/dalmatian-wearing-thundershirt.jpg?v=1635206832",
+      },
+      {
+        id: 2,
+        title: "2 post",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        img: "https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg",
+      },
+      {
+        id: 3,
+        title: "3 post",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        img: "https://cdn.shopify.com/s/files/1/0526/8740/5250/products/thundercap-calming-cap-action3.jpg?v=1640720343",
+      },
+    ],
+  }),
 };
 </script>
