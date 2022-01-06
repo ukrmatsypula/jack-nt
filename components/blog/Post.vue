72f658936cb162b4e9e9c9abc2d8d85f0b29f5fb
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-06 23:52:25 +0200

    feat: add blog/post.vue component

---
+++ components/blog/Post.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="post">
+    <div class="container">
+      <div class="post-header">
+        <img :src="post.img" :alt="post.title" />
+        <h1 class="title">{{ post.title }}</h1>
+        <p>{{ post.description }}</p>
+      </div>
+
+      <div class="post-body">
+        <p>{{ post.content }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    type: Object,
+    required: true,
+  },
+};
+</script>
+
+<style></style>
--- pages/blog/_id/index.vue
+++ pages/blog/_id/index.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="wrapper-content wrapper-content--fixed">
-    <section class="post">
-      <div class="container">
-        <div class="post-header">
-          <img :src="post.img" :alt="post.title" />
-          <h1 class="title">{{ post.title }}</h1>
-          <p>{{ post.description }}</p>
-        </div>
 
-        <div class="post-body">
-          <p>{{ post.content }}</p>
-        </div>
-      </div>
-    </section>
     <h2>post id: {{ this.$route.params.id }}</h2>
   </div>
 </template>
