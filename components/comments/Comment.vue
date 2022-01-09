5bd3ba3dd6430e7870b6fd0b35a80dc644b2b5eb
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-09 22:44:39 +0200

    feat: render comments in our post

---
+++ components/comments/Comment.vue
@@ -0,0 +1,43 @@
+<template>
+  <section v-if="comments.length" class="comments">
+    <div class="container">
+      <h2 class="title">Comments</h2>
+      <div class="comment" v-for="comment in comments" :key="comment.id">
+        <p class="name">{{ comment.name }}</p>
+        <p class="text">{{ comment.text }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Comment",
+  props: {
+    comments: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.comments {
+  text-align: center;
+  margin: 30px auto;
+
+}
+.comment {
+  width: 600px;
+  padding: 20px;
+  margin-bottom: 20px;
+  background: #fff;
+
+  .name {
+    margin-bottom: 12px;
+    font-size: 24px;
+    color: #5670e6;
+  }
+}
+</style>
--- pages/blog/_id/index.vue
+++ pages/blog/_id/index.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="wrapper-content wrapper-content--fixed">
     <Post :post="post" />
+    <Comment :comments="comments" />
     <NewComment />
     <h2>post id: {{ this.$route.params.id }}</h2>
   </div>
 </template>
 
 <script>
-import Post from '@/components/blog/Post.vue';
-import NewComment from '@/components/comments/NewComment.vue';
+import Post from "@/components/blog/Post.vue";
+import NewComment from "@/components/comments/NewComment.vue";
+import Comment from "@/components/comments/Comment.vue";
 
 export default {
   components: {
     Post,
     NewComment,
+    Comment,
   },
   data: () => ({
     post: {
@@ -24,6 +27,18 @@ export default {
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga error consequuntur minima quibusdam molestias! Excepturi, mollitia voluptatibus dolores, expedita modi eos ipsa officiis repudiandae accusamus sed explicabo recusandae, minus exercitationem sapiente quas animi cumque labore aliquid nam perspiciatis! Hic quam numquam tempora, earum temporibus et dolor minima magnam amet distinctio, nihil beatae ad odio, itaque nam dolore pariatur nesciunt architecto accusamus? Molestias officiis dolorem necessitatibus fugit eligendi tempore facere quas nesciunt laudantium adipisci dignissimos ad qui, aut rem debitis ipsum molestiae distinctio sequi quos! Est quasi dicta, eligendi tempore nemo perferendis eius magnam perspiciatis cupiditate, suscipit quis excepturi. Autem, reiciendis?",
       img: "https://cdn.shopify.com/s/files/1/0526/8740/5250/products/dalmatian-wearing-thundershirt.jpg?v=1635206832",
     },
+    comments: [
+      {
+        id: 1,
+        name: "Roman",
+        text: "Test Roman comment lorem ipsum dolor emet sit elit comment!!!",
+      },
+      {
+        id: 2,
+        name: "Alex",
+        text: "Test Alex comment lorem ipsum dolor emet sit elit comment!!!",
+      },
+    ],
   }),
 };
 </script>
