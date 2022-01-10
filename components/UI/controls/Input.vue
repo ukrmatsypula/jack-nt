3f98427d15ae69c1eac0e50fe2551a1f45df3adc
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-10 14:58:45 +0200

    refactor: craete components input and textarea

---
+++ components/UI/controls/Input.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="control">
+    <label
+      ><slog />
+      <input
+        :type="type"
+        v-bind="$attrs"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ components/UI/controls/TextArea.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="control">
+    <label
+      ><slog />
+      <text-area :value="value" @input="$emit('input', $event.target.value)">
+      </text-area>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style></style>
