9b51fc4fcbf174b91c06c64ac1c41b48d19f3e16
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-13 01:15:48 +0200

    feat: add page not-found

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="not-found">
+    <div class="container">
+      <div class="not-found__wrapper">
+        <p class="title">Page Not Found</p>
+        <nuxt-link to="/">Back to main page</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.not-found__wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 80vh;
+}
+</style>
