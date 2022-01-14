6a3600ca8ac5c3095cee4967bfee57364b909360
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-14 23:11:44 +0200

    feat: add new-post page and render form NewPostForm.vue

---
+++ components/admin/NewPostForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="new-post">
+    <div class="container">
+      <form @submit.prevent>
+        <AppInput v-model="post.title">Title:</AppInput>
+        <AppInput v-model="post.description">Description:</AppInput>
+        <AppInput v-model="post.img">Img Link:</AppInput>
+        <AppTextArea v-model="post.content">Content</AppTextArea>
+
+        <div class="controls">
+          <AppButton @click="onCancel" class="btnDanger">Cancel</AppButton>
+          <AppButton @click="onSubmit">Save</AppButton>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "NewPostForm",
+  data: () => ({
+    post: {
+      title: "",
+      description: "",
+      img: "",
+      content: "",
+    },
+  }),
+  methods: {
+    onCancel() {
+      this.$router.push("/admin/");
+    },
+    onSubmit() {
+      this.$emit("onSubmit", this.post);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.controls {
+  text-align: center;
+  margin: 20px 0;
+}
+</style>
---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NewPostForm @onSubmit="onSubmit" />
+  </div>
+</template>
+
+<script>
+import NewPostForm from "@/components/admin/NewPostForm.vue";
+
+export default {
+  components: {
+    NewPostForm,
+  },
+  layout: "admin",
+  methods: {
+    onSubmit(newPost) {
+      console.log(newPost);
+    },
+  },
+};
+</script>
+
+<style></style>
