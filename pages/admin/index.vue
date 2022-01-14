8ce159f74e52937cf2921690fccfc4a81bee1206
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-14 21:32:07 +0200

    feat: add admin layout and add page admin

---
+++ pages/admin/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <PostsList :posts="posts" />
+</template>
+
+<script>
+export default {
+  layout: "admin",
+  data: () => ({
+    posts: [
+      {
+        id: 1,
+        title: "1 post",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        img: "https://cdn.shopify.com/s/files/1/0526/8740/5250/products/dalmatian-wearing-thundershirt.jpg?v=1635206832",
+      },
+      {
+        id: 2,
+        title: "2 post",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        img: "https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg",
+      },
+      {
+        id: 3,
+        title: "3 post",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        img: "https://cdn.shopify.com/s/files/1/0526/8740/5250/products/thundercap-calming-cap-action3.jpg?v=1640720343",
+      },
+    ],
+  }),
+};
+</script>
