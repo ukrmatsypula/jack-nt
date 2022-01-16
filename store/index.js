50c4848038360d49229932bf4a7578e7ba94c5b4
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-16 19:36:02 +0200

    feat: add acion nuxtServerInit

--- store/index.js
+++ store/index.js
@@ -4,23 +4,45 @@ export const state = () => ({
   postsLoaded: [],
 });
 
-export const getters = {};
+export const getters = {
+  ;
 
 export const mutations = {
-  addPost(state, post) {
-    console.log(post);
+  addPost(state, posts) {
+    return state.postsLoaded = posts;
+  },
+  setPosts(state, posts) {
     return state.postsLoaded.push(post);
   },
 };
 
 export const actions = {
+  async nuxtServerInit({ commit }, context) {
+    return await axios
+      .get(
+        "https://blog-nuxt-5e003-default-rtdb.europe-west1.firebasedatabase.app/post.json"
+      )
+      .then((response) => {
+        const postsArray = [];
+
+        for (let key in response.data) {
+          postsArray.push({ ...response.data[key], id: key });
+          console.log(key);
+        }
+
+        commit("setPosts", postsArray);
+      })
+      .catch((error) => console.log(error));
+  },
   async addPost({ commit }, post) {
     return await axios
       .post(
         "https://blog-nuxt-5e003-default-rtdb.europe-west1.firebasedatabase.app/post.json",
         post
       )
-      .then((response) => commit("addPost", { id: response.data.name, ...post }))
+      .then((response) =>
+        commit("addPost", { id: response.data.name, ...post })
+      )
       .catch((error) => console.log(error));
   },
 };
