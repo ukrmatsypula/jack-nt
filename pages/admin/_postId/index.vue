372f6c60ba1e4de1c99fdf14c2355aae58b66744
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-16 21:46:21 +0200

    feat: add edit post without save

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -5,30 +5,30 @@
 </template>
 
 <script>
+import axios from "axios";
 import NewPostForm from "@/components/admin/NewPostForm.vue";
 
 export default {
   components: {
     NewPostForm,
   },
+  async asyncData(context) {
+    return await axios
+      .get(
+        `https://blog-nuxt-5e003-default-rtdb.europe-west1.firebasedatabase.app/post/${context.params.postId}.json`
+      )
+      .then((response) => {
+        return {
+          post: { ...response.data, id: context.params.postId },
+        };
+      })
+      .catch((err) => context.error(err));
+  },
   layout: "admin",
-  data: () => ({
-    post: {
-      id: 1,
-      title: "1 post",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga error consequuntur minima quibusdam molestias! Excepturi, mollitia voluptatibus dolores, expedita modi eos ipsa officiis repudiandae accusamus sed explicabo recusandae, minus exercitationem sapiente quas animi cumque labore aliquid nam perspiciatis! Hic quam numquam tempora, earum temporibus et dolor minima magnam amet distinctio, nihil beatae ad odio, itaque nam dolore pariatur nesciunt architecto accusamus? Molestias officiis dolorem necessitatibus fugit eligendi tempore facere quas nesciunt laudantium adipisci dignissimos ad qui, aut rem debitis ipsum molestiae distinctio sequi quos! Est quasi dicta, eligendi tempore nemo perferendis eius magnam perspiciatis cupiditate, suscipit quis excepturi. Autem, reiciendis?",
-      img: "https://cdn.shopify.com/s/files/1/0526/8740/5250/products/dalmatian-wearing-thundershirt.jpg?v=1635206832",
-    },
-  }),
   methods: {
-    onSubmit(newPost) {
-      console.log("Post Editing");
-      console.log(newPost);
+    onSubmit(post) {
+      this.$store.dispatch("editPost", post);
     },
   },
 };
 </script>
-
-<style></style>
