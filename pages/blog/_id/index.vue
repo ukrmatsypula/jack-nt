3cc5041a40a1e058fc15c3640fc42e1255b193bf
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-20 23:18:54 +0200

    feat: render comments

--- pages/blog/_id/index.vue
+++ pages/blog/_id/index.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import Post from "@/components/blog/Post.vue";
 import NewComment from "@/components/comments/NewComment.vue";
 import Comment from "@/components/comments/Comment.vue";
@@ -18,28 +19,26 @@ export default {
     NewComment,
     Comment,
   },
-  data: () => ({
-    post: {
-      id: 1,
-      title: "1 post",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga error consequuntur minima quibusdam molestias! Excepturi, mollitia voluptatibus dolores, expedita modi eos ipsa officiis repudiandae accusamus sed explicabo recusandae, minus exercitationem sapiente quas animi cumque labore aliquid nam perspiciatis! Hic quam numquam tempora, earum temporibus et dolor minima magnam amet distinctio, nihil beatae ad odio, itaque nam dolore pariatur nesciunt architecto accusamus? Molestias officiis dolorem necessitatibus fugit eligendi tempore facere quas nesciunt laudantium adipisci dignissimos ad qui, aut rem debitis ipsum molestiae distinctio sequi quos! Est quasi dicta, eligendi tempore nemo perferendis eius magnam perspiciatis cupiditate, suscipit quis excepturi. Autem, reiciendis?",
-      img: "https://cdn.shopify.com/s/files/1/0526/8740/5250/products/dalmatian-wearing-thundershirt.jpg?v=1635206832",
-    },
-    comments: [
-      {
-        id: 1,
-        name: "Roman",
-        text: "Test Roman comment lorem ipsum dolor emet sit elit comment!!!",
-      },
-      {
-        id: 2,
-        name: "Alex",
-        text: "Test Alex comment lorem ipsum dolor emet sit elit comment!!!",
-      },
-    ],
-  }),
+
+  async asyncData(context) {
+    let [posts, comments] = await Promise.all([
+      axios.get(
+        `https://blog-nuxt-5e003-default-rtdb.europe-west1.firebasedatabase.app/post/${context.params.id}.json`
+      ),
+      axios.get(
+        `https://blog-nuxt-5e003-default-rtdb.europe-west1.firebasedatabase.app/comments.json`
+      ),
+    ]);
+
+    const convertedComments = Object.keys(comments.data).map(
+      (key) => comments.data[key]
+    );
+
+    return {
+      post: posts.data,
+      comments: convertedComments,
+    };
+  },
 };
 </script>
 
